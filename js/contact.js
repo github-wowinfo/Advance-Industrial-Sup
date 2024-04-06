81d7a95165a90c0e02b72ee5c32107b43f486be7
zeba-a <you@example.com>
2024-04-06 09:51:28 +0530

    add new para

--- js/contact.js
+++ js/contact.js
@@ -35,22 +35,22 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
+                    required: "Please Enter Your Name",
                     minlength: "your name must consist of at least 2 characters"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
+                    required: "Subject Required",
                     minlength: "your subject must consist of at least 4 characters"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
+                    required: "Enter Your Phone Number",
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Enter Email Id"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
+                    required: "You have to write something to send this form.",
                     minlength: "thats all? really?"
                 }
             },
